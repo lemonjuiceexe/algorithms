0c41fd74fe8337d0b4047ca1f7aab41bbc87516a
lemonjuiceexe <74901431+lemonjuiceexe@users.noreply.github.com>
2023-09-21 18:28:26 +0200

    Full solution for spo (100/100)

--- problems/spo.cpp
+++ problems/spo.cpp
@@ -2,22 +2,45 @@
 
 #include<iostream>
 
-int line(int n)
+int fib_results[59] = {0};
+
+// int line(int n){
+//     if(n == 1 || n == 2) return n;
+//     return line(n - 1) + line(n - 2);
+// }
+
+int fib_mod10(int n)
 {
-    if(n == 1 || n == 2) return n;
-    return line(n - 1) + line(n - 2);
-}
-int circle(int n)
-{
-    if(n == 1 || n == 2) return n;
-    return 2 * line(n - 2) + line(n - 1); 
+    if (n == 1 || n == 2) return n;
+    if(fib_results[n] != 0) return fib_results[n];
+    else if(fib_results[n] == 0 && fib_results[n - 1] != 0)
+    {
+        fib_results[n] = (fib_results[n - 2] + fib_results[n - 1]) % 10;
+        return fib_results[n];
+    }
+    else if(fib_results[n] == 0 && fib_results[n - 1] == 0)
+    {
+        fib_results[n - 1] = fib_mod10(n - 1) % 10;
+        fib_results[n] = (fib_results[n - 2] + fib_results[n - 1]) % 10;
+        return fib_results[n];
+    }
 }
 
 int main()
 {
-    int n;
-    std::cin >> n;
-    std::cout << circle(n) % 10;
+    fib_results[1] = 1; fib_results[2] = 2;
+
+    fib_mod10(60);
+    // for(int i = 3; i < 60; ++i) 
+    //     std::cout 
+    //         << i << ".: " 
+    //         << fib_results[i] << ": " 
+    //         << fib_mod10(i) << " | " 
+    //         << (2 * fib_mod10((i - 2) % 60) + fib_mod10((i - 1) % 60)) % 10 
+    //         << std::endl;
+
+    int n; std::cin >> n;
+    std::cout << (2 * fib_mod10((n - 2) % 60) + fib_mod10((n - 1) % 60)) % 10;
 
     return EXIT_SUCCESS;
 }
