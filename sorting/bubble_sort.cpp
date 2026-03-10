c1dd3a452d4c07a3e7e71913187a02dcee508ea9
lemonjuiceexe <74901431+lemonjuiceexe@users.noreply.github.com>
2026-03-10 23:07:36 +0100

    Add bubble sort

---
+++ sorting/bubble_sort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+void bubble_sort(int* array, int n){
+    bool sorted = false;
+    for(int i = 0; i < n && !sorted; ++i){
+        sorted = true;
+        for(int j = 0; j < n - i - 1; ++j){
+            if(array[j] > array[j + 1]){
+                std::swap(array[j], array[j + 1]);
+                sorted = false;
+            }
+        }
+    }
+}
+
+int main(){
+    int input[10] = {2, 7, -20, 100, 13, 16, 1, 3, 2, 33};
+
+    bubble_sort(input, 10);
+    
+    for(int number : input){
+        std::cout << number << " ";
+    }
+
+    std::cin >> std::ws;
+    return EXIT_SUCCESS;
+}
